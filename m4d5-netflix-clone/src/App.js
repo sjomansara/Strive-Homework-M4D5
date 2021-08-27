98f7c3a25804c5d589f064f488a07ed689b5e624
jhowards <github@jameshoward.uk>
2021-08-27 11:37:58 +0100

    RowComponent

--- m4d5-netflix-clone/src/App.js
+++ m4d5-netflix-clone/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import RowComponent from "./components/RowComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RowComponent movie="Harry Potter" />
+      <RowComponent movie="Lord Of The Rings" />
+      <RowComponent movie="Mission Impossible" />
     </div>
   );
 }
