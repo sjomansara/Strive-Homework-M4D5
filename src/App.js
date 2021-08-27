e4a89e0f63ddbac5a196b85f2dc1344e4f9946e4
Tarunsai Sadu <tarunsai.rct@gmail.com>
2021-08-27 13:40:36 +0200

    working singlecomponent

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SingleMovie from "./components/SingleMovie";
+
+function App() {
+  return (
+    <div className="App">
+      <SingleMovie id="tt1201607" />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SingleMovie.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Card } from "react-bootstrap";
+
+class SingleMovie extends React.Component {
+  state = {
+    Poster: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "http://www.omdbapi.com/?apikey=2cd49fbb&i= " + this.props.id
+      );
+      console.log(response);
+      let image = await response.json();
+      console.log(image);
+      this.setState({ Poster: image.Poster });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.state.Poster} />
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default SingleMovie;
