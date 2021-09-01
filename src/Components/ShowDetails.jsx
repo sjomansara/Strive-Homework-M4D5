0e17fc0e5c559520aaefe0a2fd3024ba3bb1538c
sjomansara <sara.sjoman@hotmail.com>
2021-09-01 16:42:28 +0200

    decided to keep spinner and alert

--- src/Components/ShowDetails.jsx
+++ src/Components/ShowDetails.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import { Card } from "react-bootstrap"
+import { Card, Spinner, Alert } from "react-bootstrap"
 
 const ShowDetails = (props) => {
 
     const [movieDetails, setMovieDetails] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
     
+
     useEffect( async () => {
         try {
-            let response = await fetch("http://www.omdbapi.com/?apikey=2cd49fbb&i=" + props.id)
+            let response = await fetch(
+              "http://www.omdbapi.com/?apikey=2cd49fbb&i=" + props.id
+            )
             if (response.ok) {
-              console.log("Response ok!")
+              console.log("response ok!")
             let details = await response.json()
             setMovieDetails(details)
             console.log(details)
             } else {
-              console.log("Server Error")
+              console.log('Server Error')
+              setIsError(true)
+              setIsLoading(false)
             }
           } catch (error) {
             console.log(error)
+            setIsError(true)
+            setIsLoading(false)
           }
         }
     )
@@ -25,6 +34,16 @@ const ShowDetails = (props) => {
     return (
        <div>
            <h1 className="text-white">Movie Details</h1>
+           {
+                    isLoading && 
+                    <Spinner animation="border" variant="dark" className="mx-auto" />
+                }
+                  {
+                    isError &&
+                    <Alert variant="danger" className="col-2">
+                        An error occurred!
+                    </Alert>
+                }
         <Card className="col-2" id="detailsCard">
           <Card.Img id="cardImg" variant="top" src={movieDetails.Poster} className="mt-3" />
           <Card.Body className="text-white">
